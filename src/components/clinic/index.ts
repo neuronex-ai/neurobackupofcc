6c0e9f28627a1239c49a8c007574a8f6d928b687
[dyad] <suporte.neuronex@gmail.com>
2026-06-28 18:21:38 -0300

    Remove 50 files

--- src/components/clinic/index.ts
+++
@@ -1,5 +0,0 @@
-export { OrganizationSettings } from './OrganizationSettings';
-export { TeamMemberCard } from './TeamMemberCard';
-export { InviteMemberModal } from './InviteMemberModal';
-export { ClinicMetricsCards } from './ClinicMetricsCards';
-export { TeamOverviewWidget } from './TeamOverviewWidget';
